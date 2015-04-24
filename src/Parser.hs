57a6dc81301634ac696295b4bd079269c790cb51
sawaken <sasasawada@gmail.com>
2015-04-24 14:44:32 +0900

    instantize Parse Monad and MonadPlus

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,17 +1,29 @@
-{-# LANGUAGE TypeSynonymInstances #-}
-
 module Parser(topLevel) where
 
-type Parser a = String -> Maybe (a, String)
+import Control.Monad
+
+newtype Parser a = P (String -> Maybe (a, String))
 
 instance Monad Parser where
-  return :: a -> Parser a
-  return x = (\inp-> Just (x, inp))
+  -- return :: a -> Parser a
+  return x = P (\inp-> Just (x, inp))
 
-  (>>=) :: Parser a -> (a -> Parser b) -> Parser b
-  p >>= f = (\inp-> case p inp of
-                      Just (x, rest) -> f x rest
-                      Nothing -> Nothing)
+  -- (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+  p >>= f = P (\inp-> case parse p inp of
+                        Just (x, rest) -> parse (f x) rest
+                        Nothing -> Nothing)
+
+instance MonadPlus Parser where
+  mzero = P (\inp-> Nothing)
+  mplus p q = P (\inp-> case parse p inp of
+                          Just (x, rest) -> Just (x, rest)
+                          Nothing -> parse q inp)
+
+parse :: Parser a -> String -> Maybe (a, String)
+parse (P p) inp = p inp
+
+(+++) :: Parser a -> Parser a -> Parser a
+p +++ q = p `mplus` q
 
 topLevel :: Int
 topLevel = 1
